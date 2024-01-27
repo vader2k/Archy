5cad71cfff28f9446b36055fb9f21a2ef8006016
danie odukwe <kingdanny295@gmail.com>
2024-01-27 14:13:30 +0100

    setting up texts

---
+++ src/constants/data.js
@@ -0,0 +1,32 @@
+export const navlinks = [
+    {
+        id: 1,
+        title: 'Home',
+        path: '/'
+    },
+    {
+        id: 2,
+        title: 'About Us',
+        path: '/about'
+    },
+    {
+        id: 3,
+        title: 'Services',
+        path: '/services'
+    },
+    {
+        id: 4,
+        title: 'Projects',
+        path: '/projects'
+    },
+    {
+        id: 5,
+        title: 'News',
+        path: '/news'
+    },
+    {
+        id: 6,
+        title: 'Contact',
+        path: '/contact'
+    },
+]
