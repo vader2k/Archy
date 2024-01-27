ed5c6caf535b7c436898298079840464c6a84c6e
danie odukwe <kingdanny295@gmail.com>
2024-01-27 14:00:21 +0100

    done setting up nav routes

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import AboutUs from './AboutUs'
+import Contact from './Contact'
+import Home from './Home'
+import News from './News'
+import Projects from './Projects'
+import Services from './Services'
+
+export {AboutUs, Contact, Home, News, Projects, Services}
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,8 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home, AboutUs, Services, Projects, News, Contact } from '../pages'
 
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="/about" element={<AboutUs />}/>
+          <Route path="/services" element={<Services />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/news" element={<News />}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
